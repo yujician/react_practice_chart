6611af8373df8c8b1ea1ef18eef41cc61029e186
Sumi <yujician.sumi@gmail.com>
2025-01-03 00:17:33 +0900

    tailwindをビルドする設定へ変更3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 
 import '@/styles/globals.css';
-import '/styles.css';
 
 function MyApp({ Component, pageProps: { ...pageProps } }: AppProps): JSX.Element {
   return (
